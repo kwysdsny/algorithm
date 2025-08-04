53c598179505524750688b7362d19173b420b1c1
qihuiycp <2578632377@qq.com>
2025-08-04 14:26:28 +0800

    qihui

--- java/高频sql50/aaa.sql
+++ java/高频sql50/aaa.sql
@@ -151,6 +151,13 @@ FROM
      GROUP BY num
      HAVING COUNT(num) = 1) AS t
 
+-- 一些题目可以去使用having语句
+SELECT customer_id
+FROM Customer
+WHERE product_key IN (SELECT product_key FROM Product)
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product);
+
 
 
 
