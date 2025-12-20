71619249f621bcfec87e7373d9f2f6eb64f45903
ycp <2578632377@qq.com>
2025-12-20 14:48:42 +0800

    算法

---
+++ java/hot100/有效的括号.java
@@ -0,0 +1,31 @@
+package hot100;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class 有效的括号 {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (c == ')' && top != '(') {
+                    return false;
+                }
+                if (c == ']' && top != '[') {
+                    return false;
+                }
+                if (c == '}' && top != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
