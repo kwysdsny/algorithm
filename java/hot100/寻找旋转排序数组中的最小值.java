6b1b79c10c805f7eed616dca149874db09ea60ae
ycp <2578632377@qq.com>
2026-01-11 14:40:50 +0800

    算法

---
+++ java/hot100/寻找旋转排序数组中的最小值.java
@@ -0,0 +1,17 @@
+package hot100;
+
+public class 寻找旋转排序数组中的最小值 {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (nums[pivot] < nums[high]) {
+                high = pivot;
+            } else {
+                low = pivot + 1;
+            }
+        }
+        return nums[low];
+    }
+}
