c9defced8a76769e8521ffea959e358280ae06c0
ycp <2578632377@qq.com>
2026-01-14 14:22:36 +0800

    算法

---
+++ java/hot100/爬楼梯.java
@@ -0,0 +1,13 @@
+package hot100;
+
+public class 爬楼梯 {
+    public int climbStairs(int n) {
+        int p = 0, q = 0, r = 1;
+        for (int i = 1; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
+}
