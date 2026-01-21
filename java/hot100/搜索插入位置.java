f08a6a615bdac8eeb1996cc38d77db43b39f7544
ycp <2578632377@qq.com>
2026-01-21 20:04:45 +0800

    算法

---
+++ java/hot100/搜索插入位置.java
@@ -0,0 +1,18 @@
+package hot100;
+
+public class 搜索插入位置 {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1, ans = n;
+        while (left <= right) {
+            int mid = ((right - left) >> 1) + left;
+            if (target <= nums[mid]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
